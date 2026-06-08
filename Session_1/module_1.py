d411d88504e58b03e87910623d0e7020bbeb9ee1
Muhammad_Suvonov <diksend32@gmail.com>
2026-06-08 19:52:18 +0500

    Implement multiple tasks in module_1.py

--- Session_1/module_1.py
+++ Session_1/module_1.py
@@ -2,35 +2,67 @@ from typing import List
 
 
 def task_1(array: List[int], target: int) -> List[int]:
-    """
-    Write your code below
-    """
-    pass
+    r = {}
+    for i, n in enumerate(sample):
+    # enumerate -- return the value and index
+        if target - n in r:
+            return[n, target - n]
+        else:
+             r[n] = i
 
 
 def task_2(number: int) -> int:
-    """
-    Write your code below
-    """
-    pass
+    if number < 0:   # for sign
+        sign = -1
+    else:
+        sign = 1
+
+    number = abs(number)
+    result = 0
+
+    while number > 0:
+        digit = number % 10
+        result = result * 10 + digit
+        number = int(number / 10)
 
 
 def task_3(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    d = {}
+    for i in sample:
+        if i not in d.keys():
+            d[i] = 1
+        else:
+            return i
+    return -1
 
 
 def task_4(string: str) -> int:
-    """
-    Write your code below
-    """
-    pass
+    roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    total = 0
+
+    for i in range(len(string)):
+        current_value = roman[string[i]]
+
+        if i + 1 < len(string) and current_value < roman[string[i + 1]]:
+            total -= current_value
+        else:
+            total += current_value
+
+    return total
 
 
 def task_5(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    m = array[0]
+    for i in range(1, len(array)-1):
+        if m > sample[i]:
+            m = sample[i]
+    return m
