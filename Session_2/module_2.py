2b130d15e341951d0bd86939d0fee51e4082055c
Muhammad_Suvonov <diksend32@gmail.com>
2026-06-09 20:39:09 +0500

    Fix formatting of else statement in module_2.py

--- Session_2/module_2.py
+++ Session_2/module_2.py
@@ -8,7 +8,7 @@ def task_1(data_1: Dict[str, int], data_2: Dict[str, int]):
     for i in data_1.keys():
         if i in data_2.keys():
             r[i] = data_1[i] + data_2[i]
-        else :
+        else:
             r[i] = data_1[i]
     for i in data_2.keys():
         if i not in data_1.keys():
