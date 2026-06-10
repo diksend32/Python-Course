9cca794f6481a8308d794ecacbc33ae0e16e434c
Muhammad_Suvonov <diksend32@gmail.com>
2026-06-10 18:23:07 +0500

    Add multiple tasks and decorators in module_3.py
    Implemented power function, argument printing, and timing decorator. Added matrix transposition function and modified task_3.

--- Session_3/module_3.py
+++ Session_3/module_3.py
@@ -5,19 +5,29 @@ Matrix = List[List[int]]
 
 
 def task_1(exp: int):
-    pass
+    def power(x):
+        return x ** exp
+    return power
 
 
 def task_2(*args, **kwags):
-    pass
+    for value in args:
+        print(value)
+
+    for value in kwargs.values():
+        print(value)
 
 
 def helper(func):
-    pass
+    def wrapper(*args, **kwargs):
+        print("Hi, friend! What's your name?")
+        func(*args, **kwargs)
+        print("See you soon!")
+    return wrapper
 
 
 @helper
-def task_3(name: str):
+def task_3(name):
     print(f"Hello! My name is {name}.")
 
 
@@ -26,12 +36,34 @@ def timer(func):
 
 
 @timer
-def task_4():
-    return len([1 for _ in range(0, 10**8)])
+import time
 
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
 
-def task_5(matrix: Matrix) -> Matrix:
-    pass
+        result = func(*args, **kwargs)
+
+        end_time = time.time()
+        run_time = end_time - start_time
+
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+        return result
+
+    return wrapper
+
+def task_5(matrix):
+    result = []
+
+    for col in range(len(matrix[0])):
+        row = []
+
+        for r in range(len(matrix)):
+            row.append(matrix[r][col])
+
+        result.append(row)
+
+    return result
 
 
 def task_6(queue: str):
