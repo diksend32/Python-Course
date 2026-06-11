f54071e27001253bab533e8655e3afd579e3439c
Muhammad_Suvonov <diksend32@gmail.com>
2026-06-11 20:00:31 +0500

    Fix trainee class point management and methods

--- Session_4/module_4.py
+++ Session_4/module_4.py
@@ -1,48 +1,50 @@
 PASSING_GRADE = 8
 
 
-class Trainee: 
+class Trainee:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.visited_lectures = 0
         self.done_home_tasks = 0
-        self.missed_home_task = 0
+        self.missed_home_tasks = 0
         self.missed_lectures = 0
         self.mark = 0
 
     def visit_lecture(self):
         self.visited_lectures += 1
+        self._add_points(1)
 
     def do_homework(self):
-        self.visited_lectures += 2
+        self.done_home_tasks += 1
+        self._add_points(2)
 
     def miss_lecture(self):
-        self.missed_lectures -= 2
+        self.missed_lectures += 1
+        self._subtract_points(2)
 
     def miss_homework(self):
-        self.missed_home_task -= 2
+        self.missed_home_tasks += 1
+        self._subtract_points(2)
 
     def _add_points(self, points):
+        self.mark += points
         if self.mark > 10:
             self.mark = 10
-        else:
-            self.mark += points
 
     def _subtract_points(self, points):
-        if self.mark > 0:
+        self.mark -= points
+        if self.mark < 0:
             self.mark = 0
-        else:
-            self.mark -= points
 
     def is_passed(self):
-        if self.mark >= 8:
-            print('Good job!')
+        if self.mark >= PASSING_GRADE:
+            print("Good job!")
         else:
-            print(f"You need to get {8 - self.mark} more points. Try to do your best!")
+            print(f"You need {PASSING_GRADE - self.mark} more points.")
 
     def __str__(self):
-        status = (
+        return (
             f"Trainee {self.name.title()} {self.surname.title()}:\n"
             f"done homework {self.done_home_tasks} points;\n"
             f"missed homework {self.missed_home_tasks} points;\n"
