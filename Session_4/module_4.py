90226a904d05360e9604a13276bc813c0fa23a02
Muhammad_Suvonov <diksend32@gmail.com>
2026-06-11 19:44:52 +0500

    Add functionality to Trainee class methods
    Implemented methods for lecture attendance and homework completion in the Trainee class.

--- Session_4/module_4.py
+++ Session_4/module_4.py
@@ -2,29 +2,47 @@ PASSING_GRADE = 8
 
 
 class Trainee:
+    
     def __init__(self, name, surname):
-        pass
+        self.name = name
+        self.surname = surname
+        self.visited_lectures = 0
+        self.done_home_tasks = 0
+        self.missed_home_task = 0
+        self.missed_lectures = 0
+        self.mark = 0
 
-    def visit_lecture(self):
-        pass
+    def visit_lecture(self):   
+        self.visited_lectures += 1
 
     def do_homework(self):
-        pass
+        self.visited_lectures += 2
 
     def miss_lecture(self):
-        pass
+        self.missed_lectures -= 2
 
     def miss_homework(self):
-        pass
+        self.missed_home_task -= 2
 
-    def _add_points(self, points: int):
-        pass
+    def _add_points(self, points):
+        if self.mark > 10:
+            self.mark = 10
+        else:
+            self.mark += points
 
     def _subtract_points(self, points):
-        pass
+            if self.mark > 0:
+                self.mark = 0
+            else:
+                self.mark -= points
+
 
     def is_passed(self):
-        pass
+        if self.mark >= 8:
+            print('Good job!')
+        else:
+            print(f"You need to get {8 - self.mark} more points. Try to do your best!")
+
 
     def __str__(self):
         status = (
